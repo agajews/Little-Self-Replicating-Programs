fa6788559ea72405f681002dd09fc9f4ceb7e615
agajews <agajews@gmail.com>
2019-12-15 19:10:12 -0500

    added mutations

--- src/Rep.hs
+++ src/Rep.hs
@@ -47,6 +47,10 @@ throw = Thread . lift . throwError
 pause :: Thread ()
 pause = Thread $ suspend $ Identity $ return ()
 
+fromIntVal :: Value -> Int
+fromIntVal (IntVal x) = x
+fromIntVal _ = undefined
+
 runThread :: WorldState -> Thread a -> (Either EvalError (Either (Thread a) a), WorldState)
 runThread state (Thread t) = unwrapId $ runIdentity $ runStateT (runExceptT $ resume t) state where
     unwrapId (Right (Left (Identity t)), state) = (Right $ Left $ Thread t, state)
@@ -60,17 +64,116 @@ randRange range = do
     put $ state { randomGen = g }
     return x
 
+rand :: Random a => Thread a
+rand = do
+    state <- get
+    let (x, g) = random (randomGen state)
+    put $ state { randomGen = g }
+    return x
+
 randomThread :: Thread Value
 randomThread = do
     state <- get
     let univ = universe state
     let n = univSize univ
     i <- randRange (0, n - 1)
-    eval $ (Map.!) (univMap univ) i
+    eval $ univMap univ Map.! i
+
+mutateInt :: Int -> Thread Int
+mutateInt x = do
+    b <- rand
+    return $ if b then x + 1 else x - 1
+
+randInt :: Thread Int
+randInt = randRange (-5, 5)
+
+randIntVal :: Thread Value
+randIntVal = randInt >>= return . IntVal
+
+randPrimFunc :: Thread Value
+randPrimFunc = do
+    i <- randRange (0, length primFuncs - 1)
+    return $ primFuncs !! i
+
+randLambda :: Thread Value
+randLambda = do
+    x <- randInt
+    v <- randomValue
+    return $ Lambda x v
+
+randVariable :: Thread Value
+randVariable = randInt >>= return . Variable
+
+randFuncCall :: Thread Value
+randFuncCall = do
+    f <- randomValue
+    a <- randomValue
+    return $ FuncCall f a
+
+mutateInplace :: Value -> Thread Value
+mutateInplace (IntVal x) = mutateInt x >>= return . IntVal
+mutateInplace (PrimFunc _ _) = randPrimFunc
+mutateInplace (Lambda x v) = do
+    b <- rand
+    if b < mutateParP then do
+        x' <- mutateInt x
+        return $ Lambda x' v
+    else do
+        v' <- mutateInplace v
+        return $ Lambda x v'
+mutateInplace (Variable x) = mutateInt x >>= return . Variable
+mutateInplace (FuncCall f a) = do
+    b <- rand
+    if b < mutateFuncP then do
+        f' <- mutateInplace f
+        return $ FuncCall f' a
+    else do
+        a' <- mutateInplace a
+        return $ FuncCall f a'
+
+randomValue :: Thread Value
+randomValue = do
+    b <- randRange (0, 4)
+    case b :: Int of
+        0 -> randIntVal
+        1 -> randPrimFunc
+        2 -> randLambda
+        3 -> randVariable
+        4 -> randFuncCall
+
+mutateValue :: Value -> Thread Value
+mutateValue x = do
+    b <- rand
+    if b < mutateTypeP then randomValue
+    else mutateInplace x
+
+mutateP :: Double
+mutateP = 0.01
+
+mutateParP :: Double
+mutateParP = 0.2
+
+mutateFuncP :: Double
+mutateFuncP = 0.3
+
+mutateTypeP :: Double
+mutateTypeP = 0.1
+
+mutate :: Thread ()
+mutate = do
+    b <- rand
+    when (b < mutateP) $ do
+        state <- get
+        let univ = universe state
+        let n = univSize univ
+        i <- randRange (0, n - 1)
+        let m = univMap univ
+        x' <- mutateValue (m Map.! i)
+        put $ state { universe = univ { univMap = Map.insert i x' m } }
 
 runStep :: [WorldState] -> [Thread Value] -> ([WorldState], [Thread Value])
 runStep states threads = (states'', threads'') where
-    (threads', states') = unzip $ zipWith runThread states threads
+    (threads', states') = unzip [runThread s (mutate >> t) | (s, t) <- zip states threads]
     restartThread (Left err) = randomThread
     restartThread (Right (Left t)) = t
     restartThread (Right (Right _)) = randomThread
@@ -84,7 +187,7 @@ getVar :: Int -> Thread Value
 getVar x = do
     state <- get
     let (Env env) = environment state
-    case (Map.!?) env x of
+    case env Map.!? x of
         Just y -> return y
         Nothing -> throw EvalError
 
@@ -112,3 +215,6 @@ eval (FuncCall f a) = do
             pause
             return y
         _ -> throw EvalError
+
+primFuncs :: [Value]
+primFuncs = []
