0b48fb36cf3e6222bd78c38a449a14c390d21f81
agajews <agajews@gmail.com>
2019-12-16 16:04:02 -0500

    cleaned up, added functions

--- src/Rep.hs
+++ src/Rep.hs
@@ -20,17 +20,16 @@ data Value = IntVal Int
            | Variable Int
            | FuncCall Value Value
 
--- one local environment for each thread of execution
-newtype Env = Env (Map.Map Int Value)
-
--- a global universe of programs, and a size of the universe
-data Univ = Univ { univMap :: UnivMap, univSize :: Int }
-
-type UnivMap = Map.Map Int Value
+type ValueMap = Map.Map Int Value
 
 data EvalError = EvalError
 
-data WorldState = WorldState { universe :: Univ, univEdits :: UnivMap, environment :: Env, randomGen :: StdGen }
+data WorldState = WorldState { univMap :: ValueMap,
+                               univSize :: Int,
+                               univEdits :: ValueMap,
+                               envMap :: ValueMap,
+                               randomGen :: StdGen,
+                               cellPos :: Int }
 
 newtype Thread a = Thread { unwrapThread :: Coroutine Identity (ExceptT EvalError (StateT WorldState Identity)) a }
     deriving (Functor,
@@ -73,11 +72,11 @@ rand = do
 
 randomThread :: Thread Value
 randomThread = do
-    state <- get
-    let univ = universe state
-    let n = univSize univ
+    n <- getSize
     i <- randRange (0, n - 1)
-    eval $ univMap univ Map.! i
+    setCellPos i
+    cell <- getCell i
+    eval cell
 
 mutateInt :: Int -> Thread Int
 mutateInt x = do
@@ -163,13 +162,11 @@ mutate :: Thread ()
 mutate = do
     b <- rand
     when (b < mutateP) $ do
-        state <- get
-        let univ = universe state
-        let n = univSize univ
+        n <- getSize
         i <- randRange (0, n - 1)
-        let m = univMap univ
-        x' <- mutateValue (m Map.! i)
-        put $ state { universe = univ { univMap = Map.insert i x' m } }
+        x <- getCell i
+        x' <- mutateValue x
+        setCell i x'
 
 runStep :: [WorldState] -> [Thread Value] -> ([WorldState], [Thread Value])
 runStep states threads = (states'', threads'') where
@@ -178,24 +175,47 @@ runStep states threads = (states'', threads'') where
     restartThread (Right (Left t)) = t
     restartThread (Right (Right _)) = randomThread
     threads'' = map restartThread threads'
-    univ = universe $ head states
-    univ' = univ { univMap = Map.union (Map.unions $ map univEdits states') (univMap univ) }
-    updateState state = state { universe = univ', univEdits = Map.empty }
+    univ = univMap $ head states
+    univ' = Map.union (Map.unions $ map univEdits states') univ 
+    updateState state = state { univMap = univ', univEdits = Map.empty }
     states'' = map updateState states'
 
 getVar :: Int -> Thread Value
 getVar x = do
     state <- get
-    let (Env env) = environment state
-    case env Map.!? x of
+    case envMap state Map.!? x of
         Just y -> return y
         Nothing -> throw EvalError
 
 setVar :: Int -> Value -> Thread ()
 setVar x v = do
     state <- get
-    let (Env env) = environment state
-    put $ state { environment = Env $ Map.insert x v env }
+    put $ state { envMap = Map.insert x v $ envMap state }
+
+getCell :: Int -> Thread Value
+getCell x = do
+    state <- get
+    return $ univMap state Map.! x
+
+setCell :: Int -> Value -> Thread ()
+setCell x v = do
+    state <- get
+    put $ state { univMap = Map.insert x v $ univMap state }
+
+getSize :: Thread Int
+getSize = do
+    state <- get
+    return $ univSize state
+
+getCellPos :: Thread Int
+getCellPos = do
+    state <- get
+    return $ cellPos state
+
+setCellPos :: Int -> Thread ()
+setCellPos x = do
+    state <- get
+    put $ state { cellPos = x }
 
 eval :: Value -> Thread Value
 eval x@(IntVal _) = return x
@@ -217,4 +237,145 @@ eval (FuncCall f a) = do
         _ -> throw EvalError
 
 primFuncs :: [Value]
-primFuncs = []
+primFuncs = [macro3 "if" ifFunc,
+             macro2 "define" define,
+
+             func1 "peek" peek,
+             func2 "poke" poke,
+
+             func2 "+" $ intOp (+),
+             func2 "-" $ intOp (-),
+             func2 "*" $ intOp (*),
+
+             func2 ">" $ intBoolOp (>),
+             func2 "<" $ intBoolOp (<),
+             func2 "=" $ intBoolOp (==),
+
+             func2 "&&" $ boolOp (&&),
+             func2 "||" $ boolOp (||),
+
+             func1 "eval" eval,
+
+             func1 "lambda-get-var" lambdaGetVar,
+             func1 "lambda-get-val" lambdaGetVal,
+             func2 "lambda-set-var" lambdaSetVar,
+             func2 "lambda-set-val" lambdaSetVal,
+
+             func1 "funccall-get-func" funcCallGetFunc,
+             func1 "funccall-get-arg" funcCallGetArg,
+             func2 "funccall-set-func" funcCallSetFunc,
+             func2 "funccall-set-arg" funcCallSetArg]
+
+func1 :: String -> (Value -> Thread Value) -> Value
+func1 name f = PrimFunc name $ \x -> do
+    x' <- eval x
+    f x'
+
+func2 :: String -> (Value -> Value -> Thread Value) -> Value
+func2 name f = PrimFunc name $ \x -> return $ PrimFunc (name ++ "1") $ \y -> do
+    x' <- eval x
+    y' <- eval y
+    f x' y'
+
+macro2 :: String -> (Value -> Value -> Thread Value) -> Value
+macro2 name f = PrimFunc name $ \x -> return $ PrimFunc (name ++ "1") $ \y ->
+    f x y
+
+macro3 :: String -> (Value -> Value -> Value -> Thread Value) -> Value
+macro3 name f = PrimFunc name $ \x -> return $ PrimFunc (name ++ "1") $ \y -> return $ PrimFunc (name ++ "2") $ \z ->
+    f x y z
+
+-- if
+ifFunc :: Value -> Value -> Value -> Thread Value
+ifFunc b thenExpr elseExpr = do
+    b' <- eval b
+    case b' of
+        IntVal x -> if x > 0
+            then eval thenExpr
+            else eval elseExpr
+        _ -> throw EvalError
+
+-- define
+define :: Value -> Value -> Thread Value
+define (Variable x) y = do
+    y' <- eval y
+    setVar x y'
+    return y'
+define _ _ = throw EvalError
+
+-- peek
+-- poke
+peek :: Value -> Thread Value
+peek (IntVal x) = do
+    n <- getSize
+    y <- getCellPos
+    getCell ((x + y) `mod` n)
+peek _ = throw EvalError
+
+poke :: Value -> Value -> Thread Value
+poke (IntVal x) val = do
+    y <- getCellPos
+    n <- getSize
+    setCell ((x + y) `mod` n) val
+    return val
+poke _ _ = throw EvalError
+
+-- +
+-- -
+-- *
+intOp :: (Int -> Int -> Int) -> Value -> Value -> Thread Value
+intOp op (IntVal x) (IntVal y) = return $ IntVal $ op x y
+intOp _ _ _ = throw EvalError
+
+-- >
+-- <
+-- =
+intBoolOp :: (Int -> Int -> Bool) -> Value -> Value -> Thread Value
+intBoolOp op (IntVal x) (IntVal y) = return $ IntVal $ if op x y then 1 else 0
+intBoolOp _ _ _ = throw EvalError
+
+-- &&
+-- ||
+boolOp :: (Bool -> Bool -> Bool) -> Value -> Value -> Thread Value
+boolOp op (IntVal x) (IntVal y) = return $ IntVal $ if op (x > 0) (y > 0) then 1 else 0
+boolOp _ _ _ = throw EvalError
+
+-- lambda-get-var
+-- lambda-get-val
+-- lambda-set-var
+-- lambda-set-val
+lambdaGetVar :: Value -> Thread Value
+lambdaGetVar (Lambda x _) = return $ Variable x
+lambdaGetVar _ = throw EvalError
+
+lambdaGetVal :: Value -> Thread Value
+lambdaGetVal (Lambda _ y) = return y
+lambdaGetVal _ = throw EvalError
+
+lambdaSetVar :: Value -> Value -> Thread Value
+lambdaSetVar (Lambda _ y) (Variable x) = return $ Lambda x y
+lambdaSetVar _ _ = throw EvalError
+
+lambdaSetVal :: Value -> Value -> Thread Value
+lambdaSetVal (Lambda x _) y = return $ Lambda x y
+lambdaSetVal _ _ = throw EvalError
+
+-- funccall-get-func
+-- funccall-get-arg
+-- funccall-set-func
+-- funccall-set-arg
+funcCallGetFunc :: Value -> Thread Value
+funcCallGetFunc (FuncCall f _) = return f
+funcCallGetFunc _ = throw EvalError
+
+funcCallGetArg :: Value -> Thread Value
+funcCallGetArg (FuncCall _ a) = return a
+funcCallGetArg _ = throw EvalError
+
+funcCallSetFunc :: Value -> Value -> Thread Value
+funcCallSetFunc (FuncCall _ a) f = return $ FuncCall f a
+funcCallSetFunc _ _ = throw EvalError
+
+funcCallSetArg :: Value -> Value -> Thread Value
+funcCallSetArg (FuncCall f _) a = return $ FuncCall f a
+funcCallSetArg _ _ = throw EvalError
